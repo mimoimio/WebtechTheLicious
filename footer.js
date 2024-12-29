68b2d649785d6a2881e755bcc2cf2aeb1f8354cd
mimoimio <129141433+mimoimio@users.noreply.github.com>
2024-12-30 06:04:55 +0800

    index.html completed menu teaser unfinished: whatsapp link, footer,

---
+++ footer.js
@@ -0,0 +1,21 @@
+export default function footer() {
+    return (`
+        <footer>
+            <div class="container">
+                <h1>TheLicious</h1>
+                <div class="item">
+                    <a href="index.html">Home</a>
+                    <a href="about.html">About Us</a>
+                    <a href="menu.html">Menu</a>
+                    <a href="menu.html">Contact Us</a>
+                </div>
+                <div class="item">
+                    <div class="d-flex flex-row align-items-center">
+                        <a href="" class="text-center"><i class="bi bi-instagram"></i></a>
+                        <a href="" class="text-center"><i class="bi bi-whatsapp"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>`
+    )
+}
