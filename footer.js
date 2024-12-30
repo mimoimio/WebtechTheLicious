988bcd9d21beae7c1828f873f30dfe40d45a63fd
mimoimio <129141433+mimoimio@users.noreply.github.com>
2024-12-30 08:15:41 +0800

    merged aboutus.html, contactus.html

--- footer.js
+++ footer.js
@@ -11,8 +11,18 @@ export default function footer() {
                 </div>
                 <div class="item">
                     <div class="d-flex flex-row align-items-center">
-                        <a href="" class="text-center"><i class="bi bi-instagram"></i></a>
-                        <a href="" class="text-center"><i class="bi bi-whatsapp"></i></a>
+                    <a href="https://www.facebook.com/thelicious.hq/?locale=ms_MY" target="_blank">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com/thelicious_hq/" target="_blank">
+                        <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=0183518044" target="_blank">
+                        <i class="bi bi-whatsapp"></i>
+                    </a>
+                    <a href="https://t.me/theliciousiium" target="_blank">
+                        <i class="bi bi-telegram"></i>
+                    </a>
                     </div>
                 </div>
             </div>
